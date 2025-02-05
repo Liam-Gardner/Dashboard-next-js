fcb23f5f79152afc002293a6262e9c764c38b1b0
Liam-Gardner <liam.gardner@protonmail.com>
2025-02-05 14:53:28 +0000

    move query provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import { DarkModeProvider } from '@/providers/DarkModeProvider';
 import '@/styles/globals.css';
 import Layout from '@/ui/Layout';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps<{ businessName: string }>) {
   return (
-    <DarkModeProvider>
-      <Layout businessName={pageProps.businessName}>
-        <Component {...pageProps} />
-      </Layout>
-    </DarkModeProvider>
+    <QueryClientProvider client={queryClient}>
+      <DarkModeProvider>
+        <Layout businessName={pageProps.businessName}>
+          <Component {...pageProps} />
+        </Layout>
+      </DarkModeProvider>
+    </QueryClientProvider>
   );
 }
